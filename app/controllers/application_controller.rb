96c2b8b3bc9f81b4fa567fd4665442c7d4c43cb1
samyounger <sam.younger@gmail.com>
2017-04-22 13:23:54 +0100

    working on jwt authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+
+  require 'json_web_token'
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def login
+    user = User.find_by(email: params[:email].to_s.downcase)
+
+    if user && user.authenticate(params[:password])
+      if user.confirmed_at?
+      auth_token = JsonWebToken.encode({user_id: user.id})
+      render json: {auth_token: auth_token}, status: :ok
+      else
+        render json: {error: 'Email not verified' }, status: :unauthorized
+      end
+    else
+      render json: {error: 'Invalid username / password'}, status: :unauthorized
+    end
+  end
+
   private
 
   def user_params
