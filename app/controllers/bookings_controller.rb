c74d37fb501440fb300cf8dd7b41e892fa9e4b4b
samy <sam.younger@gmail.com>
2017-08-20 17:01:02 +0100

    added automatic user setup on creating a new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,20 +1,23 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_request, only: [ :create ]
   before_action :find_user, only: [ :create ]
 
   def show
-    @booking = Booking.find(params[:id])
+    @booking = @current_user.bookings.find(params[:id])
 
     render json: @booking, status: :ok
   end
 
   def create
-      @booking = @user.bookings.new(booking_params)
+    @booking = @user.bookings.new(booking_params)
+    user_to_render = @user.as_json(only: [:name, :email, :phone_number])
 
-      if @booking.save
-        render json: @booking, status: :created, location: @booking
-      else
-        render json: @booking.errors, status: :unprocessable_entity
-      end
+    if @booking.save
+      UserMailer.welcome_email(@user, password, @booking).deliver_later
+      render json: { booking: @booking, user: user_to_render}, status: :created
+    else
+      render json: @booking.errors, status: :unprocessable_entity
+    end
   end
 
   private
@@ -24,7 +27,17 @@ class BookingsController < ApplicationController
   end
 
   def find_user
-    @user = User.find_by_email(params[:email])
+    @user ||= User.find_by_email(params[:user][:email])
+    @user.update(user_params) if @user
+    @user ||= User.create(user_params) unless @user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :phone_number).merge(password: password, password_confirmation: password)
+  end
+
+  def password(pass_code = nil)
+    @password ||= SecureRandom.hex(12) unless pass_code
   end
 
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
 class UserMailer < ApplicationMailer
 
-  def welcome_email(user, password)
+  def welcome_email(user, password, booking)
     @user     = user
+    @booking  = booking
     @url      = "http://thekamesneuk.com/login/?email=#{@user.email}&password=#{password}"
     mail(to: @user.email, subject: 'Welcome to The Neuk')
   end
