df5a306b74bb511a48fae826b46ec559b1a6dd9a
Sam Younger <sam.younger@gmail.com>
2017-10-02 21:01:22 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var webpack = require("webpack");
+var path    = require("path");
+
+module.exports = {
+  entry: [
+    "script!jquery/dist/jquery.min.js",
+    'script!foundation-sites/dist/js/foundation.min.js',
+    "./app/app.jsx"
+  ],
+  externals: {
+    jquery: "jQuery"
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      "$": "jquery",
+      "jquery": "jquery"
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: "./public/bundle.js"
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      applicationStyles: "app/styles/app.scss"
+    },
+    extensions: ["", ".js", ".jsx"]
+  },
+  module: {
+    loaders: [
+      {
+        loader: "babel-loader",
+        query: {
+          presets: [ "react", "es2015", "stage-0" ]
+        },
+        test: /\.jsx?$/,
+        excludes: /(node_modules|bower_components)/
+      },{
+        loader: 'style-loader!css-loader',
+        test: /\.css$/
+      }
+    ]
+  },
+  sassLoader: {
+    includePaths: [
+      path.resolve(__dirname, './node_modules/foundation-sites/scss')
+    ],
+  },
+  devtool: "cheap-module-eval-source-map"
+};
