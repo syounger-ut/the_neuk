c1eaa0b09038a9c94615e844764618c4c2381445
Sam Younger <sam.younger@gmail.com>
2017-10-04 23:10:24 +0100

    commit end of night

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,6 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-var {Route, Router, IndexRoute, hashHistory} = require("react-router");
+var {Route, Router, Redirect, IndexRoute, hashHistory} = require("react-router");
 
 var Main = require("Main");
 
