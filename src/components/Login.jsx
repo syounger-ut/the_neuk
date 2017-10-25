a25ec7fa3d1be8ffcca5f264d8275d51b05cd0ea
samy <sam.younger@gmail.com>
2017-10-25 22:28:24 +0100

    login/register conditional rendering, form not plugged in

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,11 +8,17 @@ class Login extends React.Component {
     super(props);
     this.state = {
       username: '',
+      first_name: '',
+      last_name: '',
+      email: '',
+      phone_number: '',
       password: '',
+      loginForm: true
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.formSwitch   = this.formSwitch.bind(this);
   }
 
   handleChange(event) {
@@ -40,14 +46,41 @@ class Login extends React.Component {
     });
 
   }
+
+  formSwitch(selectedForm) {
+    var loginForm = this.state.loginForm;
+    if(selectedForm === 'register') {
+      this.setState({
+        loginForm: false
+      });
+    } else {
+      this.setState({
+        loginForm: true
+      })
+    }
+  }
+
   render() {
     const username     = this.state.username;
+    const first_name   = this.state.first_name;
+    const last_name    = this.state.last_name;
+    const email        = this.state.email;
+    const phone_number = this.state.phone_number;
     const password     = this.state.password;
 
-    return (
-      <div className="login">
-        <h2>Login</h2>
-        <form className="loginForm" onSubmit={this.handleSubmit}>
+    const loginForm = this.formSwitch.bind(this, 'login');
+    const registerForm = this.formSwitch.bind(this, 'register');
+
+    var form;
+    if(this.state.loginForm) {
+      form = (
+        <div className="login">
+          <h2>Login</h2>
+          <ul>
+            <li onClick={loginForm}>Login</li>
+            <li onClick={registerForm}>Register</li>
+          </ul>
+          <form className="loginForm" onSubmit={this.handleSubmit}>
             <label htmlFor="username">Email:</label>
             <div className="input">
               <input
@@ -65,8 +98,72 @@ class Login extends React.Component {
                 onChange={this.handleChange}
                 placeholder="Password" />
             </div>
-          <input className="button" type="submit" value="Submit"/>
-        </form>
+            <input className="button" type="submit" value="Submit"/>
+          </form>
+        </div>
+      );
+    } else {
+      form = (
+        <div className="login">
+          <h2>Register</h2>
+          <ul>
+            <li onClick={loginForm}>Login</li>
+            <li onClick={registerForm}>Register</li>
+          </ul>
+          <form className="registerForm" onSubmit={this.handleSubmit}>
+            <label htmlFor="first_name">First Name:</label>
+            <div className="input">
+              <input
+                type="text"
+                name="first_name"
+                value={first_name}
+                onChange={this.handleChange}
+                placeholder="First Name" />
+            </div>
+            <label htmlFor="last_name">Last Name:</label>
+            <div className="input">
+              <input
+                type="text"
+                name="last_name"
+                value={last_name}
+                onChange={this.handleChange}
+                placeholder="Last Name" />
+            </div>
+            <label htmlFor="email">Email:</label>
+            <div className="input">
+              <input
+                type="text"
+                name="email"
+                value={email}
+                onChange={this.handleChange}
+                placeholder="Email" />
+            </div>
+            <label htmlFor="phone_number">Phone Number:</label>
+            <div className="input">
+              <input
+                type="text"
+                name="phone_number"
+                value={phone_number}
+                onChange={this.handleChange}
+                placeholder="Phone Number" />
+            </div>
+            <label htmlFor="password">Password:</label>
+            <div className="input">
+              <input type="password"
+                name="password"
+                value={password}
+                onChange={this.handleChange}
+                placeholder="Password" />
+            </div>
+            <input className="button" type="submit" value="Submit"/>
+          </form>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        {form}
       </div>
     );
   }
