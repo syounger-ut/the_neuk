22e3101085a9240e389c70613f1a8b16e26f1c02
samy <sam.younger@gmail.com>
2017-10-26 09:53:41 +0100

    slight reformat of login conditional rendering

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -68,101 +68,95 @@ class Login extends React.Component {
     const phone_number = this.state.phone_number;
     const password     = this.state.password;
 
-    const loginForm = this.formSwitch.bind(this, 'login');
+    const loginForm    = this.formSwitch.bind(this, 'login');
     const registerForm = this.formSwitch.bind(this, 'register');
 
     var form;
+    var loginTitle;
     if(this.state.loginForm) {
+      loginTitle = ( <h2>Login</h2> );
       form = (
-        <div className="login">
-          <h2>Login</h2>
-          <ul>
-            <li onClick={loginForm}>Login</li>
-            <li onClick={registerForm}>Register</li>
-          </ul>
-          <form className="loginForm" onSubmit={this.handleSubmit}>
-            <label htmlFor="username">Email:</label>
-            <div className="input">
-              <input
-                type="text"
-                name="username"
-                value={username}
-                onChange={this.handleChange}
-                placeholder="Username" />
-            </div>
-            <label htmlFor="password">Password:</label>
-            <div className="input">
-              <input type="password"
-                name="password"
-                value={password}
-                onChange={this.handleChange}
-                placeholder="Password" />
-            </div>
-            <input className="button" type="submit" value="Submit"/>
-          </form>
-        </div>
+        <form className="loginForm" onSubmit={this.handleSubmit}>
+          <label htmlFor="username">Email:</label>
+          <div className="input">
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={this.handleChange}
+              placeholder="Username" />
+          </div>
+          <label htmlFor="password">Password:</label>
+          <div className="input">
+            <input type="password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+              placeholder="Password" />
+          </div>
+          <input className="button" type="submit" value="Submit"/>
+        </form>
       );
     } else {
+      loginTitle = ( <h2>Register</h2> );
       form = (
-        <div className="login">
-          <h2>Register</h2>
-          <ul>
-            <li onClick={loginForm}>Login</li>
-            <li onClick={registerForm}>Register</li>
-          </ul>
-          <form className="registerForm" onSubmit={this.handleSubmit}>
-            <label htmlFor="first_name">First Name:</label>
-            <div className="input">
-              <input
-                type="text"
-                name="first_name"
-                value={first_name}
-                onChange={this.handleChange}
-                placeholder="First Name" />
-            </div>
-            <label htmlFor="last_name">Last Name:</label>
-            <div className="input">
-              <input
-                type="text"
-                name="last_name"
-                value={last_name}
-                onChange={this.handleChange}
-                placeholder="Last Name" />
-            </div>
-            <label htmlFor="email">Email:</label>
-            <div className="input">
-              <input
-                type="text"
-                name="email"
-                value={email}
-                onChange={this.handleChange}
-                placeholder="Email" />
-            </div>
-            <label htmlFor="phone_number">Phone Number:</label>
-            <div className="input">
-              <input
-                type="text"
-                name="phone_number"
-                value={phone_number}
-                onChange={this.handleChange}
-                placeholder="Phone Number" />
-            </div>
-            <label htmlFor="password">Password:</label>
-            <div className="input">
-              <input type="password"
-                name="password"
-                value={password}
-                onChange={this.handleChange}
-                placeholder="Password" />
-            </div>
-            <input className="button" type="submit" value="Submit"/>
-          </form>
-        </div>
+        <form className="registerForm" onSubmit={this.handleSubmit}>
+          <label htmlFor="first_name">First Name:</label>
+          <div className="input">
+            <input
+              type="text"
+              name="first_name"
+              value={first_name}
+              onChange={this.handleChange}
+              placeholder="First Name" />
+          </div>
+          <label htmlFor="last_name">Last Name:</label>
+          <div className="input">
+            <input
+              type="text"
+              name="last_name"
+              value={last_name}
+              onChange={this.handleChange}
+              placeholder="Last Name" />
+          </div>
+          <label htmlFor="email">Email:</label>
+          <div className="input">
+            <input
+              type="text"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+              placeholder="Email" />
+          </div>
+          <label htmlFor="phone_number">Phone Number:</label>
+          <div className="input">
+            <input
+              type="text"
+              name="phone_number"
+              value={phone_number}
+              onChange={this.handleChange}
+              placeholder="Phone Number" />
+          </div>
+          <label htmlFor="password">Password:</label>
+          <div className="input">
+            <input type="password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+              placeholder="Password" />
+          </div>
+          <input className="button" type="submit" value="Submit"/>
+        </form>
       );
     }
 
     return (
-      <div>
+      <div className="login">
+        {loginTitle}
+        <ul>
+          <li onClick={loginForm}>Login</li>
+          <li onClick={registerForm}>Register</li>
+        </ul>
         {form}
       </div>
     );
