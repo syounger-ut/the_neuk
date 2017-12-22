08482346d4649e5323f77de3c88bd4e62abc9e0f
samy <sam.younger@gmail.com>
2017-12-22 14:03:37 +0000

    small chg, temporary

--- app/controllers/pay_controller.rb
+++ app/controllers/pay_controller.rb
@@ -1,9 +1,15 @@
 class PayController < ApplicationController
 
   def create
+    Stripe::Charge.create(
+      :amount => 500,
+      :currency => "gbp",
+      :source => params[:pay][:id], # obtained with Stripe.js
+      :description => "Test payment"
+    )
     binding.pry
-    payment_details = { amount: params[:amount], source: params[:stripeToken, description: params[:description]] }
-    Stripe.charge(payment_details)
+    # payment_details = { amount: params[:amount], source: params[:stripeToken, description: params[:description]] }
+    # Stripe.charge(payment_details)
   end
 
 end
