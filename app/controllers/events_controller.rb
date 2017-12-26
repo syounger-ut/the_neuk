66282a65ead18648a27b38f9ec0643c15da73436
samy <sam.younger@gmail.com>
2017-12-26 19:01:44 +0000

    events controller working

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,15 +4,20 @@ class EventsController < ApplicationController
     headers  = { Authorization: "Bearer #{ENV['LIST_API_KEY']}" }
 
     # Coordinates of Kames, with 5 mile radius
-    params   = { near: [ '55.8850849', '-5.2365178' ], _distance: '5' }
-    response = HTTParty.get('https://api.list.co.uk/v1/events', headers: headers, params: params)
+    list_params = { near: '55.8850849, -5.2365178', _distance: '5', min_date: params[:start_date], max_date: params[:end_date] }
+    response = HTTParty.get('https://api.list.co.uk/v1/events', headers: headers, params: list_params)
 
     if response
-      response_serialized = response.map{ |event| EventsSerializer.new(event) }
-      render json: response_serialized, status: :ok
+      render json: response.parsed_response
     else
       render json: { errors: "Something went wrong getting events" }
     end
+  end
+
+  private
+
+  def event_params
+    params.permit(:start_date, :end_date)
   end
 
 end
