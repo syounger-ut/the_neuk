1506524d09fe5d26adcfe28eeef3d98407119032
samy <sam.younger@gmail.com>
2017-12-26 14:10:03 +0000

    sorting user bookings desc

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,10 @@ class UserSerializer < ActiveModel::Serializer
   has_many :bookings
   attributes :id, :full_name, :first_name, :last_name, :email, :phone_number
 
+  def bookings
+    object.bookings.order("start_date DESC")
+  end
+
   def full_name
     "#{object.first_name} #{object.last_name}"
   end
