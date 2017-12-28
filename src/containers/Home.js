e78ae757ac35b5f002d1a94c00c055a2815fe55d
samy <sam.younger@gmail.com>
2017-12-28 16:37:08 +0000

    layout of home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -28,6 +28,13 @@ class Home extends Component {
 
     return (
       <div className="home">
+        <div className="home-image">
+          <h1>The Neuk</h1>
+        </div>
+        <div className="home-description">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
         <Calendar
           className="calendar"
           booking={booking}
