9c3297a40f097e1c2874d9ca103e498c9ea0daef
samy <sam.younger@gmail.com>
2017-12-28 13:29:46 +0000

    styling events

--- src/components/Booking/Events.js
+++ src/components/Booking/Events.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
 
+import moment from 'moment';
+
 class Events extends Component {
   render() {
     let events;
     if(this.props.events) {
       events = Object.entries(this.props.events).map(([key, event]) => {
+        console.log(event);
         return (
-          <ul key={key}>
-            <li>{event.name}</li>
-            <li>{event.descriptions[0].description}</li>
-          </ul>
+          <li key={key} className="event">
+            <p>Image</p>
+            <img src={ event.images ? event.images[0].url : ""}/>
+            <p>Name</p>
+            <p>{event.name}</p>
+            <p>Description</p>
+            <p>{event.descriptions[0].description}</p>
+            <p>Start</p>
+            <p>{moment(event.schedules[0].start_ts).format('Do MMMM YYYY, h:mm a')}</p>
+            <p>End</p>
+            <p>{moment(event.schedules[0].end_ts).format('Do MMMM YYYY, h:mm a')}</p>
+            <p>Price</p>
+            <p>{event.schedules[0].ticket_summary}</p>
+            <p>Addess Name</p>
+            <p>{event.schedules[0].place.name}</p>
+            <p>Addess</p>
+            <p>{event.schedules[0].place.address}</p>
+            <p>Addess Town</p>
+            <p>{event.schedules[0].place.town}</p>
+            <p>Addess Postcode</p>
+            <p>{event.schedules[0].place.post_code}</p>
+            <p>Website</p>
+            <p><a href={event.website}>{event.website}</a></p>
+          </li>
         );
       });
     }
     return (
-      <section className="events">
+      <ul className="events">
         {events}
-      </section>
+      </ul>
     );
   }
 }
