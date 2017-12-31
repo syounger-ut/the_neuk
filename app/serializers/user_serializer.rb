370491a0dcfdc2a19ba96355fdf6ef1a1d99d829
samy <sam.younger@gmail.com>
2017-12-31 12:58:19 +0000

    role added to user return

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   has_many :bookings
-  attributes :id, :full_name, :first_name, :last_name, :email, :phone_number
+  attributes :id, :full_name, :first_name, :last_name, :email, :phone_number, :role
 
   def bookings
     object.bookings.order("start_date DESC")
