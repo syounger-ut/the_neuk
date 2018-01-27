3b04295bec76547103d599b2319e3b8acf0a6e41
samy <sam.younger@gmail.com>
2018-01-27 14:26:05 +0000

    restyled nav, and fixed list_api being called in infinite loop

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -6,16 +6,26 @@ class Nav extends Component {
     let user = this.props.user;
     let logout = this.props.logout
     let loginButton;
-    if (user) {
+    if (user && user.role === 'admin') {
       loginButton = (
-        <ul className="loginButton">
+        <li>
+          <Link to='/user'>{user.email}</Link>
+          <ul className="hidden">
+            <li><a href='/' onClick={logout}>Admin</a></li>
+            <li><a href='/' onClick={logout}>Logout</a></li>
+          </ul>
+        </li>
+      )
+    } else if(user) {
+      loginButton = (
+        <ul className="hidden">
           <li><Link to='/user'>{user.email}</Link></li>
           <li><a href='/' onClick={logout}>Logout</a></li>
         </ul>
       )
     } else {
       loginButton = (
-        <ul className="loginButton">
+        <ul className="hidden">
           <li><Link to='/authentication'>Login</Link></li>
         </ul>
       )
@@ -23,11 +33,14 @@ class Nav extends Component {
 
     return (
       <nav>
+        {/* THESE TWO BUTTONS ARE FOR MOBILE RESPONSIVE. NOT SET UP YET */}
+        <label for="show-menu" class="show-menu">Show Menu</label>
+        <input type="checkbox" id="show-menu" role="button"/>
         <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
-          <li><Link to='/bookings'>Bookings page</Link></li>
           {loginButton}
+          <li><Link to='/bookings'>Bookings page</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/'>Home</Link></li>
         </ul>
       </nav>
     );
