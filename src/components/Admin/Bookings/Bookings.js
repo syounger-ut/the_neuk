67272558b5d87d4df16138b06fb2802715abcd6d
samy <sam.younger@gmail.com>
2018-07-29 14:52:38 +0100

    started admin bookings

--- src/components/Admin/Bookings/Bookings.js
+++ src/components/Admin/Bookings/Bookings.js
@@ -4,6 +4,9 @@ import React, { Component } from 'react';
 
 class Bookings extends Component {
   render() {
+    const user = this.props.user;
+    const bookings = user.bookings;
+    console.log(bookings)
     return (
       <div className='admin'>
         <h1>Bookings Component</h1>
