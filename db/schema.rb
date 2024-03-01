116dc35ec387e196763efd99a6330f8532b8faf5
Sam Younger <syounger@usertesting.com>
2024-03-01 21:59:18 +0000

    Add active storage
    * Following instructions from here https://github.com/thoughtbot/paperclip/blob/main/MIGRATING.md

--- db/schema.rb
+++ db/schema.rb
@@ -2,70 +2,95 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180923162020) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_03_01_213918) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "bookings", force: :cascade do |t|
-    t.integer  "user_id"
-    t.date     "start_date"
-    t.date     "end_date"
-    t.integer  "occupants"
-    t.string   "special_instructions"
-    t.integer  "booking_source",       default: 0
-    t.boolean  "paid",                 default: false
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
-    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "images", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
-    t.datetime "photo_updated_at"
-    t.integer  "location_id"
-    t.string   "imageable_type"
-    t.integer  "imageable_id"
-    t.integer  "things_to_do_id"
-    t.index ["imageable_type", "imageable_id"], name: "index_images_on_imageable_type_and_imageable_id", using: :btree
-    t.index ["location_id"], name: "index_images_on_location_id", using: :btree
-    t.index ["things_to_do_id"], name: "index_images_on_things_to_do_id", using: :btree
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "locations", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "things_to_dos", force: :cascade do |t|
+  create_table "bookings", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "occupants"
+    t.string "special_instructions"
+    t.integer "booking_source", default: 0
+    t.boolean "paid", default: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "images", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "location_id"
+    t.string "imageable_type"
+    t.integer "imageable_id"
+    t.integer "things_to_do_id"
+    t.index ["imageable_type", "imageable_id"], name: "index_images_on_imageable_type_and_imageable_id"
+    t.index ["location_id"], name: "index_images_on_location_id"
+    t.index ["things_to_do_id"], name: "index_images_on_things_to_do_id"
+  end
+
+  create_table "locations", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "things_to_dos", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.string "website_url"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "phone_number"
-    t.integer  "role",            default: 0
-    t.string   "password_digest"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone_number"
+    t.integer "role", default: 0
+    t.string "password_digest"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
