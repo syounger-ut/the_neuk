5573037b7cb75dcadf2a0694a90bbfb848a1d744
Sam Younger <syounger@usertesting.com>
2024-03-02 14:49:52 +0000

    ActiveStorage adjustments migrating from paperclip
    * Following instructions from https://edgeguides.rubyonrails.org/active_storage_overview.html

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,13 +2,9 @@ class Image < ApplicationRecord
   # Assocations
   belongs_to :imageable, polymorphic: true, optional: true
 
-  has_attached_file :photo, styles: {
-    thumb: "100x100>",
-    square: "200x200#",
-    medium: "300x300"
-  }
+  has_one_attached :photo
 
   # Validations
-  validates_attachment_presence :photo
-  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+  validates_absence_of :photo
+  validates_acceptance_of :photo, content_type: /\Aimage\/.*\z/
 end
